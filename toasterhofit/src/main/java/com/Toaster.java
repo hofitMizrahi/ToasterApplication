511a37cb4b6b1fe94b0eb16b80171dab08c1241b
hofit.m <hofit.m@meuhedet.co.il>
2019-08-30 23:13:02 +0300

    Initial commit

---
+++ toasterhofit/src/main/java/com/Toaster.java
@@ -0,0 +1,12 @@
+package com;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+}
